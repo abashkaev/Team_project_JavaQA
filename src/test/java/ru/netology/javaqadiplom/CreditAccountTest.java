258bf25cad2491e26d34b6ddf66250b8edd0ec0f
AleksandrBashkaev <Bashkaev_a@mail.ru>
2023-06-01 20:37:22 +0400

    Tests for creditAccount

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -7,14 +7,64 @@ public class CreditAccountTest {
 
     @Test
     public void shouldAddToPositiveBalance() {
-        CreditAccount account = new CreditAccount(
-                0,
-                5_000,
-                15
-        );
+        CreditAccount account = new CreditAccount(1_000, 5_000,15);
 
         account.add(3_000);
 
-        Assertions.assertEquals(3_000, account.getBalance());
+        Assertions.assertEquals(4_000, account.getBalance());
+    }
+    @Test
+    public void createAccountWithNegativeRate () {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {CreditAccount account = new CreditAccount(0, 5_000,-15);} );
+    }
+
+    @Test
+    public void byeIfAmountIsLessZero () {
+        CreditAccount account = new CreditAccount(0, 5_000,15);
+
+        Assertions.assertEquals(false, account.pay(-300));
+    }
+
+    @Test
+    public void byeIfAmountIsZero () {
+        CreditAccount account = new CreditAccount(0, 5_000,15);
+
+        Assertions.assertEquals(false, account.pay(0));
+    }
+
+    @Test
+    public void byeIfAmountIsGreaterZero () {
+        CreditAccount account = new CreditAccount(1000, 5_000,15);
+
+        account.pay(500);
+
+        Assertions.assertEquals(500, account.getBalance());
+    }
+    @Test
+    public void byeUnderCreditLimit() {
+        CreditAccount account = new CreditAccount(0, 5_000,15);
+
+        Assertions.assertEquals(false, account.pay(6000));
+        Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void creditPercent () {
+        CreditAccount account = new CreditAccount(-1000, 5_000,15);
+
+        Assertions.assertEquals(150, account.yearChange());
+    }
+    @Test
+    public void creditPercentIfPositiveBalance () {
+        CreditAccount account = new CreditAccount(1000, 5_000,15);
+
+        Assertions.assertEquals(0, account.yearChange());
+    }
+
+    @Test public void creditPercentIfBalanceIsZero () {
+        CreditAccount account = new CreditAccount(0, 5_000,15);
+
+        Assertions.assertEquals(0, account.yearChange());
     }
 }
+
