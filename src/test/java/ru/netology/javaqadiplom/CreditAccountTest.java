3a700a995f8e719547940cfc013a6b2991e308cf
AleksandrBashkaev <Bashkaev_a@mail.ru>
2023-06-03 15:35:33 +0400

    add two test

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class CreditAccountTest {
 
     @Test
-    public void shouldAddToPositiveBalance() {
+    public void shouldAddToPositiveBalance() {//добавление на счет корректного значения
         CreditAccount account = new CreditAccount(1_000, 5_000,15);
 
         account.add(3_000);
@@ -52,7 +52,7 @@ public class CreditAccountTest {
     public void creditPercent () {
         CreditAccount account = new CreditAccount(-1000, 5_000,15);
 
-        Assertions.assertEquals(150, account.yearChange());
+        Assertions.assertEquals(-150, account.yearChange());
     }
     @Test
     public void creditPercentIfPositiveBalance () {
@@ -66,5 +66,37 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(0, account.yearChange());
     }
+
+    @Test
+    public void createAccountWithRateIsZero () {
+        CreditAccount account = new CreditAccount(0, 5_000,0);
+
+        Assertions.assertEquals(0, account.getRate());
+    }
+
+    @Test
+    public void payIfCreditLimitOver () {
+        CreditAccount account = new CreditAccount(0, 5_000,15);
+
+        Assertions.assertEquals(false, account.pay(6000));
+    }
+    @Test
+    public void  balanceAfterPayIfBalanceUnderCreditLimit() { //Тест на баланс, при выходе за кредитный лимит
+        CreditAccount account = new CreditAccount(0, 5_000,15);
+
+        account.pay(6000);
+
+        Assertions.assertEquals(0, account.getBalance());
+    }
+    @Test
+    public void balanceAfterPay () {// Тест на изменение баланса, после pay
+        CreditAccount account = new CreditAccount(1500, 5_000, 15);
+
+        account.pay(500);
+
+        Assertions.assertEquals(1000, account.getBalance());
+    }
+
+
 }
 
